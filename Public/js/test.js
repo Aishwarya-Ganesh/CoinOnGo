4fbf40b1cb385d94c5492ea52fb0d7ab5d535d99
allanhm <heemin.kang@hotmail.com>
2022-03-30 00:44:43 +0900

    teest

--- Public/js/test.js
+++ Public/js/test.js
@@ -1,17 +1,17 @@
-(function() {
-  var firebase = require("firebase");
-  // Required for side-effects\
-  require("firebase/firestore");
-  // Initialize Cloud Firestore through Firebase
-  firebase.initializeApp({
-    apiKey: "AIzaSyD2vioD8VxyJb7KOTzFEDe8SQNbUoNPFlI",
-    authDomain: "coinongo-6b06e.firebaseapp.com",
-    projectId: "coinongo-6b06e",
-  });
-  var db = firebase.firestore();
-  db.collection("result").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data()}`);
+const firebase = require("firebase");
+// Required for side-effects\
+require("firebase/firestore");
+
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyD2vioD8VxyJb7KOTzFEDe8SQNbUoNPFlI",
+  authDomain: "coinongo-6b06e.firebaseapp.com",
+  projectId: "coinongo-6b06e",
+});
+
+var db = firebase.firestore();
+db.collection("result").get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`);
     });
-  });
-})
+});
