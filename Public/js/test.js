efce492e43e922c82e1bd1e91cf7aad9daf3e4fc
allanhm <heemin.kang@hotmail.com>
2022-03-29 22:44:48 +0900

    test

--- Public/js/test.js
+++ Public/js/test.js
@@ -1,12 +1,27 @@
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+import {initializeApp} from 'firebase-admin/app';
+import { initializeFirestroe, CACHE_SIZE_UNLIMITED } from 'firebase/firestore';
 
-initializeApp();
+const firebaseAPP = initializeApp ({
+    apiKey: "AIzaSyD2vioD8VxyJb7KOTzFEDe8SQNbUoNPFlI",
+    authDomain: "coinongo-6b06e.firebaseapp.com",
+    databaseURL: "https://coinongo-6b06e-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "coinongo-6b06e",
+    storageBucket: "coinongo-6b06e.appspot.com",
+    messagingSenderId: "782287423141",
+    appId: "1:782287423141:web:c47321b041969d9c0bef4c",
+    measurementId: "G-M0MF3RB55Q"
+  });
+
+
+
 
 const db = getFirestore();
 
 
-const snapshot = await db.collection('result').get();
-snapshot.forEach((doc) => {
-  console.log(doc.id, '=>', doc.data());
-});
+const snapshot = await db.collection('Wallet_DB').get();
+
+console.log(snapshot);
+
+//snapshot.forEach((doc) => {
+//  console.log(doc.id, '=>', doc.data());
+//});
